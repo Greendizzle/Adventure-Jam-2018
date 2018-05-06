8cfc9c3cb030e5e5be1e2bb5bfa9f17c5219c22d
Greendizzle <greendizzle2@gmail.com>
2018-05-06 16:43:07 -0500

    Added basic left and right movement

---
+++ Adventure Jam 2018 Unity/Adventure Jam 2018/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public int jumpForce;
+    public Rigidbody2D Rb;
+    public int MovementSpeed;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        
+
+        Rb.velocity = new Vector2(horizontal * MovementSpeed, vertical);
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            jump(); 
+        }
+    }
+
+    void jump() {
+        Rb.AddForce(new Vector2(0, jumpForce));
+        Debug.Log("Space Pressed");
+
+    }
+
+}
+
+
