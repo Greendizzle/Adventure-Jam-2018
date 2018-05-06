5498a4f20c4580df8d71477da861c4625c581465
Greendizzle <greendizzle2@gmail.com>
2018-05-06 11:07:55 -0500

    Added Left and right movement
    Added left and right movement and some of the stuff for jumping

---
+++ Adventure Jam 2018 Unity/Adventure Jam 2018/Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    
+
+    public float movementSpeed;
+    public Transform[] groundpoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        
+
+       Movement(horizontal);
+	}
+
+    private void Movement(float horizontal)
+    {
+        
+
+        rb.velocity = new Vector2(horizontal*movementSpeed, rb.velocity.y);
+    }
+}
