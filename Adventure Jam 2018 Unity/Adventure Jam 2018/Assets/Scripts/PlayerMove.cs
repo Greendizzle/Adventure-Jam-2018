609cfc836db70d643293935c6abda182c00b8b44
Mumblinmad <linusbmartin@gmail.com>
2018-05-07 16:58:16 -0500

    Movement Edit with Jumping
    Adding Jumping

--- Adventure Jam 2018 Unity/Adventure Jam 2018/Assets/Scripts/PlayerMove.cs
+++ Adventure Jam 2018 Unity/Adventure Jam 2018/Assets/Scripts/PlayerMove.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
-    public int jumpForce;
-    public Rigidbody2D Rb;
-    public int MovementSpeed;
-
+    public float speed = 5f;
+    public float jumpSpeed = 8f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void FixedUpdate() {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        
-
-        Rb.velocity = new Vector2(horizontal * MovementSpeed, vertical);
-
-
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        movement = Input.GetAxis("Horizontal");
+        if (movement > 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+        else if (movement < 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        }
     }
-
-    
 }
+
+
 
 
